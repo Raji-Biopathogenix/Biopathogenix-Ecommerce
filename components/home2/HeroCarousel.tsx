dc05fe4fdc4422b71898dc74eb3d60d676b4e952
rajeswari <rajeswariUSA@users.noreply.github.com>
2026-08-05 09:00:46 -0400

    Use validation services image for hero center block

--- components/home2/HeroCarousel.tsx
+++ components/home2/HeroCarousel.tsx
@@ -30,7 +30,7 @@ const HERO_IMAGES = [
   },
   {
     // Bottom Center (largest)
-    src: "/images/home/high-perform.png",
+    src: "/images/validation%20services/2.png",
     alt: "Gloved hands holding sample tubes",
     className:
       "left-[16%] bottom-[2%] h-[46%] sm:left-[18%] sm:bottom-[2%] sm:h-[50%] md:left-[16%] md:bottom-[4%] md:h-[53%] lg:left-[10%] lg:bottom-[7%] lg:h-[56%]",
@@ -136,3 +136,5 @@ export default function HeroCarousel({ result }: HeroCarouselprops) {
     </section>
   );
 }
+
+
